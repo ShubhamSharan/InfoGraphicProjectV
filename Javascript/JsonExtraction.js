824906c0933bcea874d5a246e511fc2f15c60831
CosmeticCatCoder <CosmeticCatCoder@users.noreply.github.com>
2018-11-16 16:55:05 -0500

    Initial commit

---
+++ Javascript/JsonExtraction.js
@@ -0,0 +1,12 @@
+{
+    "type": "Feature",
+    "properties": {
+        "description": "<strong>ST. JOSEPH BLVD @ JEANNE D'ARC BLVD</strong><p><li>54 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+        "icon": "car"
+
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.54014,45.46414]
+    }
+}
---
+++ Javascript/dataset2.js
@@ -0,0 +1,256 @@
+var datasetModule2 = {
+    "id": "places",
+    "type": "symbol",
+    "source": {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>ST. JOSEPH BLVD @ JEANNE D'ARC BLVD</strong><p><li>54 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.54014,45.46414]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HUNT CLUB RD @ RIVERSIDE DR</strong><p><li>45 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.69319,45.33461]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC119A RAMP75 & HWY417 IC118 RAMP57</strong><p><li>44 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.68175,45.41276]
+                }
+            },
+            //record 4
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC122 RAMP61 & HWY417 IC121B RAMP16</strong><p><li>44 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.71709,45.40143]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC124 RAMP65 & HWY417 IC122 RAMP51</strong><p><li>43 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.73253,45.39162]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>WEST HUNT CLUB RD @ WOODROFFE AVE</strong><p><li>41 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.74897,45.32870]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>BASELINE RD @ WOODROFFE AVE</strong><p><li>40 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.76298,45.35150]
+                }
+            },
+            //element 8
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>ST. JOSEPH BLVD/OLD MONTREAL RD @ TRIM RD</strong><p><li>40 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.47803,45.48892]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>SHIGHWAY 417 btwn HWY417 IC127 RAMP25 & HWY417 IC126 RAMP51</strong><p><li>39 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.75894,45.36614]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC118 RAMP67 & HWY417 IC117 RAMP36</strong><p><li>37 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.66604,45.41695]
+                }
+            },
+            //Record 11
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HAWTHORNE RD @ HUNT CLUB RD</strong><p><li>34 total collisions</li><li>No cyclists were involved.</li><li>1 pedistrian was involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.59724,	45.37403]
+                }
+            },
+            //Record 12
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>GREENBANK RD @ STRANDHERD DR</strong><p><li>33 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.74639,45.27053]
+                }
+            },
+            //Record 13
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>CYRVILLE RD @ INNES RD</strong><p><li>32 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.60245,45.41648]
+                }
+            },
+            //Record 14
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>DONALD ST @ ST. LAURENT BLVD</strong><p><li>32 total collisions</li><li>No cyclist were involved</li><li>1 pedistrian was involved.</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.64000,45.42895]
+                }
+            },
+            //Record 15
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC122 RAMP15 & HWY417 IC121B RAMP51</strong><p><li>32 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.71702,	45.40125]
+                }
+            },
+            //Record 16
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>BASELINE RD @ MERIVALE RD</strong><p><li>31 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.73082,45.36528]
+                }
+            },
+            //Record 17
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC126 RAMP61 & HWY417 IC124 RAMP76</strong><p><li>31 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.74856,45.37605]
+                }
+            },
+            //Record 18
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>MERIVALE RD @ WEST HUNT CLUB RD</strong><p><li>31 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.72411,45.334805]
+                }
+            },
+            //Record 19
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC124 RAMP67 & HWY417 IC123 RAMP62</strong><p><li>29 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.73487,45.38892]
+                }
+            },
+            //Record 20
+            {
+                "type": "Feature",
+                "properties": {
+                    "description": "<strong>HIGHWAY 417 btwn HWY417 IC138 RAMP61 & HWY417 IC134 RAMP36</strong><p><li>29 total collisions</li><li>No pedistrians and cyclists were involved</li></p>",
+                    "icon": "car"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-75.86124,45.33059]
+                }
+            }
+        ] //termination
+        }
+    },
+    "layout": {
+        "icon-image": "{icon}-15",
+        "icon-allow-overlap": true
+    }
+}
